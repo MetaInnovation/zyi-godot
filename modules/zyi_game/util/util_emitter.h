8aecbd6836fd9c371e8e0f77754bf2d09cdc9d49
maple <2361017046@qq.com>
2025-05-16 18:09:31 +0800

    feat(zyi_game): ZyiUtilEmitter支持指定p_unique_key

--- modules/zyi_game/util/util_emitter.h
+++ modules/zyi_game/util/util_emitter.h
@@ -21,6 +21,7 @@ private:
 	struct InternalListenerItem {
 		Callable callback;
 		bool once = false;
+		String unique_key = "";
 		bool invalid = false;
 	};
 	HashMap<StringName, std::vector<InternalListenerItem>> listener_map;
@@ -30,11 +31,11 @@ protected:
 
 public:
 	bool check_has_listener(const StringName &p_name) const;
-	void on(const StringName &p_name, const Callable &p_callback);
-	void _on(const StringName &p_name, const Callable &p_callback, bool p_once);
-	void off(const StringName &p_name, const Callable &p_callback);
+	void on(const StringName &p_name, const Callable &p_callback, String p_unique_key = "");
+	void _on(const StringName &p_name, const Callable &p_callback, bool p_once, String p_unique_key = "");
+	void off(const StringName &p_name, const Callable &p_callback, String p_unique_key = "");
 	bool off_all(const StringName &p_name);
-	void once(const StringName &p_name, const Callable &p_callback);
+	void once(const StringName &p_name, const Callable &p_callback, String p_unique_key = "");
 	void emit(const StringName &p_name, const Variant &p_payload = Variant());
 	void clear_listeners_map(const StringName &p_name);
 	void clear();
