474f8cf35a5dc40bd267ebe5ceb359cc6f6d5dda
maple <2361017046@qq.com>
2025-05-16 13:26:34 +0800

    fix(zyi_game): 修复ZyiNodePoolHelper::release_node传参和node is tree问题

--- modules/zyi_game/pool/node_pool_helper.h
+++ modules/zyi_game/pool/node_pool_helper.h
@@ -28,7 +28,7 @@ public:
 	static void handle_lazy_pool_reready(Node *node);
 	static Node *prepare_node_by_scene(const Ref<PackedScene> &scene, Ref<ZyiNodePool> pool);
 	static void release_node(Node *node, Ref<ZyiNodePool> pool);
-	static void lazy_release_pool_node(Node *node, Ref<ZyiNodePool> pool, Ref<ZyiUtilCallableObject> p_callable_obj = nullptr);
+	static void lazy_release_pool_node(Node *node, Ref<ZyiNodePool> pool, Ref<ZyiUtilCallableObject> p_callable_obj);
 };
 
 #endif /* NODE2D_POOL_HELPER_H */
